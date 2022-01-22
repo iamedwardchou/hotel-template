8f20434586be5c13d8ea3f6d4bcd910fe48f1a0e
sheng-wei12 <a15361222@gmail.com>
2022-01-22 23:45:38 +0800

    style changed

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,11 +1,77 @@
 import React from 'react'
+import NavBar from '../components/nav/Nav'
+import Footer from '../components/nav/Footer'
+import {Container, Row, Col, Button} from 'react-bootstrap';
+import about1 from '../assets/about1.jpg'
+import about2 from '../assets/about2.jpg'
+import about3 from '../assets/about3.jpg'
+import './About.css';
 
-const About = (props) => {
-    console.log('props in About', props);
+
+const AboutUs = () =>{
+
+    return(
+        <>
+        <section className="about-us">
+            <Container className="py-lg-5 py-sm-4">
+                <Row>
+                    <Col md={6}>
+                        <h3>
+                        Best luxury Hotel in London since 2000. Our hotel has been present for over 20 years.
+                        </h3>
+                    </Col>
+                    <Col md={6}>
+                        <p>
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+                        Fugit tempore sapiente, distinctio perferendis voluptas consequatur quaerat incidunt. 
+                        Similique, officia! Dolorum fugiat et aliquam necessitatibus quas reiciendis, 
+                        totam voluptatibus deleniti tempore doloribus dolor modi, ab facilis quam officiis ipsam doloremque.
+                        <br/>                       
+                        <br/>
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+                        Doloribus veritatis incidunt iste totam, consequuntur minus assumenda commodi sint asperiores. 
+                        Voluptatibus incidunt alias.
+                        </p>
+                    </Col>
+                </Row>
+                <ul className="d-flex mt-5">
+                    <li className="about-pic">
+                        <img calssName="rounded" alt ="" src={about1}></img>
+                    </li>
+                    <li className="about-pic">                    
+                        <img calssName="rounded" alt ="" src={about2}></img>
+                    </li>
+                    <li className="about-pic">
+                        <img calssName="rounded" alt ="" src={about3}></img>                    
+                    </li>
+                </ul>
+            </Container>
+            <div className="about-us-bg">
+                <div className="about-bg">
+                    <div className="d-flex flex-column align-items-center filter">
+                        <h4 className="mb-md-5 mb-4">Doing the right thing, at the right time</h4>
+                        <Row>
+                            <Col md={6}>D</Col>
+                            <Col md={6}>u</Col>
+                            <Col md={6}>s</Col>
+                            <Col md={6}>t</Col>
+                        </Row>
+                    </div>
+                </div>
+            </div>
+        </section>
+        </>
+    )
+}
+
+
+const About = () => {
     return (
-        <div>
-            About
-        </div>
+        <>
+        <NavBar/>
+        <AboutUs/>
+        <Footer/>
+        </>
     )
 }
 
